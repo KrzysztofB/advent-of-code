e7c8f971c8c3d4b7a8fb1c6dcdd211016bc0d8b0
Krzysztof Bardzinski <krzysztof.bardzinski@allegro.pl>
2021-12-15 23:56:03 +0100

    2021 day 14 fast

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,93 +1,87 @@
-
 fun main() {
-    fun load(input: List<String>): Pair<String,Map<Pair<Char,Char>,Char>> {
+    fun load(input: List<String>): Pair<String,Map<String,Char>> {
         val template = input[0].trim()
 
-        val insertionRules = mutableMapOf<Pair<Char,Char>,Char>()
+        val insertionRules = mutableMapOf<String,Char>()
         for(lineIdx in 2.until(input.size)){
             val line = input[lineIdx]
             val (ruleFrom,ruleTo) = line.replace(" -> ",":").split(":")
-            val pair = ruleFrom[0] to ruleFrom[1]
-            insertionRules.put(pair, ruleTo[0])
+            insertionRules.put(ruleFrom, ruleTo[0])
         }
         return template to insertionRules
     }
 
-    fun incCharCount( _key:Char, value:Long?) = if(value==null) 1L else value+1L
+    fun <T> incCount( _key:T, value:Long?) = if(value==null) 1L else value+1L
 
-    fun applyRules(template: String, insertionRules: Map<Pair<Char,Char>, Char>):String {
+    fun doubledFinalCount(template: String, pairCount: MutableMap<String, Long>):Map<Char,Long> {
+        val charCounter = (pairCount.asSequence()
+            .flatMap { sequenceOf(it.key[0] to it.value, it.key[1] to it.value) }
+                + sequenceOf(template.first() to 1L, template.last() to 1L))
+            .groupingBy { it.first }
+            .fold(0L, { acc:Long, charCount: Pair<Char,Long> -> acc+charCount.second} )
+        return charCounter
+    }
 
-        val nextTemplate = StringBuffer()
-        var previous = '@'
-        for(i in 0.until(template.length)){
-            val c = template[i]
-            try {
-                if (i == 0) {
-                    nextTemplate.append(c)
-                    continue
-                }
+    fun finalResult(charCounts: Map<Char,Long>): Long {
+        val sorted = charCounts.asSequence().sortedBy { it.value }
+        return sorted.last().value/2L - sorted.first().value/2L
+    }
 
-                val toInsert = insertionRules.getOrDefault(previous to c, '@')
-                if (toInsert != '@') {
-                    nextTemplate.append(toInsert)
-                }
-                nextTemplate.append(c)
-            }finally {
-                previous = c
+    fun updateCounter(provious:Long?, update:Long):Long? =
+        ((provious?:0L)+update).let { if(it==0L) null else it }
+
+
+    fun applyRules(pairCount: MutableMap<String, Long>, insertionRules: Map<String, Char>) {
+        val change= mutableListOf<Pair<String, Long>>()
+        insertionRules.asSequence().forEach {
+            val count = pairCount.get(it.key)?: 0L
+            if(count>0){
+                change.add(Pair( String(charArrayOf(it.key[0], it.value)),count))
+                change.add(Pair( String(charArrayOf(it.value, it.key[1])),count))
+                change.add(Pair( it.key, -count))
             }
         }
 
-        return nextTemplate.toString()
+        change.forEach{
+            pairCount.compute(it.first, { _:String, value:Long? -> updateCounter(value, it.second) })
+        }
     }
 
-
-
-    fun calcValue(template: CharSequence): Int {
-        val sorted = template.groupingBy { it }.eachCount().toList().sortedBy { it.second }
-        return sorted.last().second - sorted.first().second
-    }
-
-
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val (template, insertionRules) = load(input)
-        //println(template)
-        var nextTemplate = template
+        val pairCount = mutableMapOf<String,Long>()
+        for(i in 1.until(template.length)){
+            val pair = template.substring(i-1, i+1)
+            pairCount.compute(pair, ::incCount)
+        }
         repeat(10){
-            nextTemplate = applyRules(nextTemplate, insertionRules)
-            //println(nextTemplate)
+            applyRules(pairCount, insertionRules)
         }
-        return calcValue(nextTemplate)    }
+        val charCounts = doubledFinalCount(template, pairCount)
 
-    fun applyAndCount(c0: Char, c1: Char, counters: MutableMap<Char, Long>, insertionRules: Map<Pair<Char,Char>, Char>,level: Int) {
-        if(level==0) return
-        val toInsert = insertionRules.getOrDefault(c0 to c1, '@')
-        if (toInsert == '@') {
-            return
-        }
-        applyAndCount(c0, toInsert, counters, insertionRules, level-1)
-        counters.compute(toInsert, ::incCharCount)
-        applyAndCount(toInsert,c1, counters, insertionRules, level-1)
+        return finalResult(charCounts)
     }
 
     fun part2(input: List<String>): Long {
-        val counters = mutableMapOf<Char,Long>()
         val (template, insertionRules) = load(input)
-        template.forEach { counters.compute(it, ::incCharCount)
-        }
+        val pairCount = mutableMapOf<String,Long>()
         for(i in 1.until(template.length)){
-            applyAndCount(template[i-1], template[i], counters, insertionRules, 40)
+            val pair = template.substring(i-1, i+1)
+            pairCount.compute(pair, ::incCount)
         }
-        val sorted = counters.toList().map{it.second}.sorted()
-        return sorted.last() - sorted.first()
+        repeat(40){
+            applyRules(pairCount, insertionRules)
+        }
+        val charCounts = doubledFinalCount(template, pairCount)
 
+        return finalResult(charCounts)
     }
-
     // test if implementation meets criteria from the description, like:
-    check(part1(readInput("Day14_test")) == 1588)
-
+    check(part1(readInput("Day14_test")) == 1588L)
+    //check(part1(readInput("Day14")) == 3408L)
     check(part2(readInput("Day14_test")) == 2188189693529L)
-//
-      //println(part1(readInput("Day14")))
+
+    println(part1(readInput("Day14")))
     println(part2(readInput("Day14")))
 
 }
