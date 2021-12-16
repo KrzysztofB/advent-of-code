06f9d71e01325bc49ab88e3eaca0f9ca6aa680d0
Krzysztof Bardzinski <krzysztof.bardzinski@allegro.pl>
2021-12-16 02:06:29 +0100

    2021 day 15

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,79 +1,98 @@
-import java.lang.Integer.min
+class Dijkstra(val grid: Array<IntArray>, val multiply: Int) {
+    val width = grid[0].size
+    val height = grid.size
+
+    val explored = mutableSetOf<Pair<Int, Int>>()
+    val frontier = mutableSetOf<Pair<Int, Int>>()
+
+    fun gridValue(cords: Pair<Int, Int>): Int {
+        val row = cords.first % height
+        val col = cords.second % width
+        return (grid[row][col] + cords.first / height + cords.second / width).let { if (it > 9) it - 9 else it }
+    }
+
+    private val distanceToNodeField =
+        Array(multiply * height) { rowIdx -> IntArray(multiply * width) { Int.MAX_VALUE } }
+
+    fun distanceToNode(node: Pair<Int, Int>): Int = distanceToNodeField[node.first][node.second]
+
+    fun distanceToNode(node: Pair<Int, Int>, value: Int) {
+        distanceToNodeField[node.first][node.second] = value
+    }
+
+
+    fun minPathDijkstra(): Int {
+        val startNode = Pair(0, 0)
+        val endNode = Pair(multiply * height - 1, multiply * width - 1)
+
+        distanceToNode(startNode, 0)
+
+        frontier.add(startNode)
+        while (frontier.isNotEmpty()) {
+            val nearestNode = frontier.minByOrNull { node -> distanceToNode(node) }!!// priority queue :)
+            frontier.remove(nearestNode)
+            explored.add(nearestNode)
+
+            if (nearestNode == endNode) {
+                return distanceToNode(nearestNode)
+            }
+
+            neighbours(nearestNode).filter { it !in explored }
+                .forEach {
+                    val alt = distanceToNode(nearestNode) + gridValue(it)
+                    if (alt < distanceToNode(it)) {
+                        distanceToNode(it, alt)
+                    }
+                    if (it !in frontier) {
+                        frontier.add(it)
+                    }
+                }
+        }
+        return 0
+    }
+
+    private fun neighbours(node: Pair<Int, Int>) = sequenceOf(
+        node.first - 1 to node.second,
+        node.first + 1 to node.second,
+        node.first to node.second - 1,
+        node.first to node.second + 1
+    ).filter { it.first >= 0 && it.first < multiply * height }
+        .filter { it.second >= 0 && it.second < multiply * width }
+
+}
 
 fun main() {
     fun loadGrid(input: List<String>): Array<IntArray> {
         val width = input[0].trim().length
         val height = input.size
-        val grid = Array<IntArray>(height) { lineIdx ->
+        val grid = Array(height) { lineIdx ->
             IntArray(width) { colIdx ->
-                     Character.getNumericValue(input[lineIdx][colIdx])
+                Character.getNumericValue(input[lineIdx][colIdx])
             }
         }
         return grid
     }
 
-
-    fun printDigitGrid(grid: Array<IntArray>, msg: String = "") {
-        println("--- $msg ---")
-        for (row in grid) {
-            for (col in row) {
-                print(col.toString(16))
-            }
-            println()
-        }
-    }
-    fun printGrid(grid: Array<IntArray>, msg: String = "") {
-        println("--- $msg ---")
-        for (row in grid) {
-            for (col in row) {
-                print(col.toString().padStart(4))
-            }
-            println()
-        }
-    }
-
-    //FAIL: counts only path DOWN & RIGHT
-    fun cheapestPath(grid: Array<IntArray>){
-        val maxCol = grid[0].indices.last
-        val maxRow = grid.indices.last
-        for(row in maxRow downTo 0)
-        for(col in maxCol downTo 0)
-        {
-            val minCost =
-                when{
-                    (row==maxRow)&& (col==maxCol) -> 0
-                    else -> min(
-                        if(row==maxRow) Int.MAX_VALUE else grid[row+1][col],
-                        if(col==maxCol) Int.MAX_VALUE else grid[row][col+1]
-                    )
-                }
-
-            if((row==0)&&(col==0)) {
-                grid[row][col] = minCost
-            } else{
-                grid[row][col] += minCost
-            }
-        }
-    }
-
     fun part1(input: List<String>): Int {
         val grid = loadGrid(input)
-        printGrid(grid)
-        cheapestPath(grid)
-        printGrid(grid)
+        val dijkstra = Dijkstra(grid, 1)
+        val result = dijkstra.minPathDijkstra()
 
-        return grid[0][0]
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        //val grid = loadGrid(input)
-        return 0
+        val grid = loadGrid(input)
+        val dijkstra = Dijkstra(grid, 5)
+        val result = dijkstra.minPathDijkstra()
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     check(part1(readInput("Day15_test")) == 40)
-    //check(part2(readInput("Day15_test")) == 195)
+    check(part2(readInput("Day15_test")) == 315)
 
     println(part1(readInput("Day15")))
-   // println(part2(readInput("Day15")))
+    println(part2(readInput("Day15")))
 }
